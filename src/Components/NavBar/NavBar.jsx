519f7845df7948a6736310b5f56edc3da129d024
Dev621-tech <devinosborne11@yahoo.com>
2026-03-05 18:10:46 -0500

    NavBar w/ Icon Update

--- src/Components/NavBar/NavBar.jsx
+++ src/Components/NavBar/NavBar.jsx
@@ -1,18 +1,35 @@
 import { Link } from "react-router";
 import "./NavBar.css";
+import star from "../../assets/star.png";
 
-export default function NavBar(){
+export default function NavBar() {
 
-    return(
+    return (
         <>
-        <header className="navbar">
-             <h1 className="navbarTitle">Accomplishment Tracker</h1>
-        <nav className="navbarAuthButtons">
-            <Link to={"/signingin"}><button>Sign In</button></Link>
-            <Link to={"/newuser"}><button>Sign Up</button></Link>
-        </nav>
-        </header>
-       
+            <header className="navbar">
+                <Link to={"/"}>
+                    <h1 className="navbarTitle">Accomplishment Tracker</h1>
+                </Link>
+                <div className="navbarButtons">
+                    <nav className="navbarUserButtons">
+                        <Link to={""}><button>HomePage</button></Link>
+                        <Link to={""}><button>Post</button></Link>
+                        <Link to={""}><button>Inspiration</button></Link>
+                    </nav>
+
+                    <div className="navbarIcon">
+                        <img src={star} alt="icon" />
+                    </div>
+
+                    <nav className="navbarAuthButtons">
+                        <Link to={""}><button>About</button></Link>
+                        <Link to={"/signingin"}><button>Sign In</button></Link>
+                        <Link to={"/newuser"}><button>Sign Up</button></Link>
+                    </nav>
+                </div>
+
+            </header>
+
         </>
     )
 }
