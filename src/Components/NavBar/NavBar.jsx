b180cb0d90d26c12b3ca524e0846359f12384e72
Dev621-tech <devinosborne11@yahoo.com>
2026-03-05 17:28:57 -0500

    NavBar Updates

--- src/Components/NavBar/NavBar.jsx
+++ src/Components/NavBar/NavBar.jsx
@@ -1,13 +1,18 @@
 import { Link } from "react-router";
+import "./NavBar.css";
 
 export default function NavBar(){
 
     return(
         <>
-        <nav>
+        <header className="navbar">
+             <h1 className="navbarTitle">Accomplishment Tracker</h1>
+        <nav className="navbarAuthButtons">
             <Link to={"/signingin"}><button>Sign In</button></Link>
             <Link to={"/newuser"}><button>Sign Up</button></Link>
         </nav>
+        </header>
+       
         </>
     )
 }
