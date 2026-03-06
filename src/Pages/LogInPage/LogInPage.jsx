c1a3020ea563692a3d23e886c55e10a106d156ee
Dev621-tech <devinosborne11@yahoo.com>
2026-03-06 14:38:25 -0500

    Successful Login ✅

--- src/Pages/LogInPage/LogInPage.jsx
+++ src/Pages/LogInPage/LogInPage.jsx
@@ -5,26 +5,61 @@ import './LogInPage.css';
 
 
 export default function LogInPage() {
-    const [ username, setUsername ] = useState("");
-    const [ passsword, setPassword ] = useState("");
+    const [username, setUsername] = useState("");
+    const [password, setPassword] = useState("");
     const nav = useNavigate();
 
-    
+    const handleSubmit = async (e) => {
+        e.preventDefault();
+        
+        try {
+
+            const response = await axios.post(`http://localhost:3000/api/user/login`, {
+                username,
+                password
+            });
+
+            console.log(response.data);
+
+            if (response.data.success){
+                nav(response.data.homepage);
+            } else {
+                alert(response.data.message)
+            }
+
+        } catch (error) {
+            console.error(error.message);
+            alert(`Login FAILED ! Please Try Again .`)
+        }
+
+    }
 
     return (
         <>
             <div className="loginContainer">
                 <fieldset>
                     <legend>Log Into Your Account</legend>
-                    <form>
+                    <form onSubmit={handleSubmit}>
                         <label>
                             Username:
-                            <input type="text" name="username" placeholder="Enter Username" required />
+                            <input 
+                            type="text" 
+                            name="username" 
+                            placeholder="Enter Username" 
+                            value={username}
+                            onChange={e => setUsername(e.target.value)}
+                            required />
                         </label>
                         <br />
                         <label>
                             Password:
-                            <input type="password" name="password" placeholder="Enter Password" required />
+                            <input 
+                            type="password" 
+                            name="password" 
+                            placeholder="Enter Password"
+                            value={password}
+                            onChange={e => setPassword(e.target.value)} 
+                            required />
                         </label>
                         <br />
                         <input type="submit" value="LOG IN" />
